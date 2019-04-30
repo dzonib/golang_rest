90b558540676cb828192324f94ec892b4030da05
dzonib <nikolabosnjak381@gmail.com>
2019-04-30 19:50:25 +0200

    simple db with gorm

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/dzonib/golang_rest/api/user"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+type User struct {
+	gorm.Model
+	Name string
+	Age  int
+}
+
 func main() {
-	// register  RESTful endpoint handler for "/users/"
-	http.Handle("/users/", &user.UserAPI{})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=dzonib dbname=gopg password=123456 sslmode=disable")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&User{})
+
+	db.Create(&User{Name: "Dzoni B", Age: 123})
+
 }
